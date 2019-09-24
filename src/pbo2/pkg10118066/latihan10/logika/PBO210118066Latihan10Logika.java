58065af08f98d66fb95c9b55f492ac3fa9037171
Tegar Lucky'Q Oakley <55655976+tegaroakley@users.noreply.github.com>
2019-09-24 15:55:16 +0900

    Add files via upload

---
+++ src/pbo2/pkg10118066/latihan10/logika/PBO210118066Latihan10Logika.java
@@ -0,0 +1,28 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package pbo2.pkg10118066.latihan10.logika;
+
+/**
+ *
+ * @author 
+ * NAMA  : Tegar Lucky'Q Oakley 
+ * NIM   : 10118066 
+ * KELAS : IF2 
+ * Deskripsi Program : Program ini berisi program untuk Membuktikan Logika Program
+ */
+public class PBO210118066Latihan10Logika {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        int umur = 19;
+        boolean dewasa;
+        dewasa = (umur > 17);
+        System.out.println("Status Dewasa : " + dewasa);
+    }
+    
+}
